59d1355aeae5b1ed048b3a1effb52ad5390fdf41
xbiotech <biotech9261@gmail.com>
2023-02-13 20:49:09 +0100

    ADD functionality to copy to clipboard button

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const inputTextArea = document.querySelector(".inputtextarea");
 const runCodeBtn = document.querySelector(".removeclones");
 const outputTextArea = document.querySelector(".outputtextarea");
-
+const copyBtn = document.querySelector(".copy");
 
 function removeClones(input){
     return [...new Set (input.split(" "))]
 }
-
-
-
-
 
 
 runCodeBtn.addEventListener("click", () => {
@@ -17,3 +13,11 @@ runCodeBtn.addEventListener("click", () => {
     const result = removeClones(input);
     outputTextArea.textContent = result.join(" ");
 })
+
+function copyToClipboard(){
+    outputTextArea.select();
+    navigator.clipboard.writeText(outputTextArea.value);
+    alert("Copied to Clipboard");
+}
+
+copyBtn.addEventListener("click", copyToClipboard);
